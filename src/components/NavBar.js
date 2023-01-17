71f83d6c4dc9f9dc5a01b99540f8d9a44713b823
joanne muthoni <joanne.mumbi@student.moringaschool.com>
2023-01-17 21:30:11 +0300

    complete

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,7 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  const listLinks= links.map((link, index) => <a href="{link}" key={link.index}> {link}</a>)
+  const listLinks= links.map((link, index) => <a href="{link}" key={index}> {link}</a>)
                 
   return (<nav>{/* display an <a> tag for each link here */}
             {listLinks}
